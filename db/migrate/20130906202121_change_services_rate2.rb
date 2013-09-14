8f6d08d0f482f138036ed6187016c5a705d7f748
Pietro Passarellie <pietro.passarelli@gmail.com>
2013-09-15 01:36:06 +0200

    decimals change

--- db/migrate/20130906202121_change_services_rate2.rb
+++ db/migrate/20130906202121_change_services_rate2.rb
@@ -1,5 +1,5 @@
 class ChangeServicesRate2 < ActiveRecord::Migration
   def change
-  		change_column(:services, :rate, :decimal, {:precision  => 8, :scale  => 2})
+  		change_column(:services, :rate, :decimal, {precision: 8, scale: 2})
   end
 end
