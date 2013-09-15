cf3957c0544c13cc0b5a1ff50b593f069a8e9b72
Pietro Passarellie <pietro.passarelli@gmail.com>
2013-09-15 11:45:49 +0200

    test

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,6 +4,8 @@ Simpleinvoice::Application.configure do
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
+  config.assets.prefix = "/dev-assets"
+
   config.cache_classes = false
 
   # Do not eager load code on boot.
