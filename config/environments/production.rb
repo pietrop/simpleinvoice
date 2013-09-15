781f1299a76ec05e563343aa689f71b2599a6ce4
Pietro Passarellie <pietro.passarelli@gmail.com>
2013-09-15 11:40:12 +0200

    test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,6 +29,8 @@ Simpleinvoice::Application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
+  config.assets.initialize_on_precompile = false
+
   # Generate digests for assets URLs.
   config.assets.digest = true
 
