191d24ce4a6b1b5cd0e3fe718473fe284c5c4039
Pietro Passarellie <pietro.passarelli@gmail.com>
2013-09-20 16:45:53 +0100

    working, added e-mail validates with devise regex

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,6 +20,7 @@ class User < ActiveRecord::Base
   validates :last_name, presence: true
   validates :address, presence: true
   validates :email, presence: true
-  validates :phone, presence: true
+  validates_format_of :email,:with => Devise.email_regexp
+  validates :phone, presence: true, numericality: true
 end
 
